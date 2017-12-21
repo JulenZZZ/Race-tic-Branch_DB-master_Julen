be651368a1e026d4635fe585327f9ffd87a31e2a
JulenZZZ <jzulaikake16dw@ikzubirimanteo.com>
2017-12-21 09:42:26 +0100

    Rutas del Admin añadidas

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'usuario' => 'App\Http\Middleware\Admin',
+        //'usuario' => 'App\Http\Middleware\UsuariosMiddleware',
     ];
 }
